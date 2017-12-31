bbe0e8b4058d3a33b6bbde6ebcfb62ec82ca6e29
kamilnaja <kamilnaja@gmail.com>
2017-12-31 22:50:00 +0100

    poprawiono progress-bar

--- web-api/src/app/progress-bar/progress-bar.component.ts
+++ web-api/src/app/progress-bar/progress-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, OnChanges, OnDestroy } from '@angular/core';
 import { QuestionsService } from '../services/questionsService';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from 'store';
+import { CounterActions } from 'app/app.actions';
 
 @Component({
     selector: 'app-progress-bar',
@@ -9,19 +12,33 @@ import { QuestionsService } from '../services/questionsService';
 
 export class ProgressBarComponent implements OnInit, OnChanges {
     questionsLength: number;
+    subscription;
+    count: number = 1;
+
     @Input()
     currentQuestion: number;
     currentLength: number;
 
     constructor(
-        public questionService: QuestionsService
-    ) { }
+        public questionService: QuestionsService,
+        private ngRedux: NgRedux<IAppState>,
+        private actions: CounterActions
+    ) {
+        this.subscription = ngRedux.select<number>('count')
+            .subscribe(newCount => this.count = newCount);
+     }
 
     ngOnInit() {
         this.questionsLength = this.questionService.getQuestionsListLength();
+        // console.log(`length: ${this.count}  QuestionLength: ${this.questionsLength} `)
+        
+    }
+    
+    ngOnChanges() {
+        // this.currentLength = this.count;
+        console.log(this.currentLength);
+        this.currentLength = Math.floor((this.count) * 100 / this.questionsLength); 
+        // console.log(`length: ${this.currentLength}  currQuestion: ${this.questionsLength} `)
     }
 
-    ngOnChanges() {
-        this.currentLength = Math.floor((this.currentQuestion) * 100 / this.questionsLength); 
-    }
 }
